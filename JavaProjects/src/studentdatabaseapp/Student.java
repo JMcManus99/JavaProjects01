f7523985fd599e974136008ca9c41c0a486eb600
jackm <jackm@192.168.1.14>
2023-10-13 13:35:11 +0100

    Name Prompt completed, ID generated with uni Year included

--- JavaProjects/src/studentdatabaseapp/Student.java
+++ JavaProjects/src/studentdatabaseapp/Student.java
@@ -1,8 +1,49 @@
 package studentdatabaseapp;
 
-public class Student {
-private void sysout() {
-	// TODO Auto-generated method stub
+import java.util.Scanner;
 
-}
+public class Student {
+
+	private String firstName;
+	private String lastName;
+	private int uniYear;
+	private String studentID;
+	private String courses;
+	private int tuitionBalance;
+	private static int costOfCourse = 600;
+	private static int id = 1000;
+	
+	//Constructor prompts users name and year
+	public Student() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		this.firstName = in.nextLine();
+		
+		System.out.print("Enter student last name:  ");
+		this.lastName = in.nextLine();
+		
+		System.out.print("1- First Year \n2- Second Year\n3- Placement\n4- Final Year\nEnter student year: ");
+		this.uniYear = in.nextInt();
+		
+		setStudentID();
+		
+		System.out.println(firstName + " " + lastName + " " + uniYear + " " + studentID);
+	}
+	//Generate an ID
+	private void setStudentID() {
+		//uni year + id
+		id++;
+		this.studentID = uniYear + "" + id;
+		
+		
+	}
+	
+	//enrol in courses
+	
+	//view balance
+	
+	//pay tuition
+	
+	//show status
+	
 }
--- JavaProjects/src/studentdatabaseapp/StudentDatabaseApp.java
+++ JavaProjects/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -3,8 +3,10 @@ package studentdatabaseapp;
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Student stu1 = new Student();
+		//ask how many users we want to ass
+		
+		//Create a number of new students
 	}
 
 }
