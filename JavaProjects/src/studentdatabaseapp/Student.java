0faffc0cb3d3a3e9dd398e0283a019f8ab51f152
jackm <jackm@DESKTOP-O38Q16C>
2023-10-23 09:52:01 +0100

    Completed Student Data base app

--- JavaProjects/src/studentdatabaseapp/Student.java
+++ JavaProjects/src/studentdatabaseapp/Student.java
@@ -8,42 +8,72 @@ public class Student {
 	private String lastName;
 	private int uniYear;
 	private String studentID;
-	private String courses;
+	private String courses = "";
 	private int tuitionBalance;
 	private static int costOfCourse = 600;
 	private static int id = 1000;
-	
-	//Constructor prompts users name and year
+
+	// Constructor prompts users name and year
 	public Student() {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter student first name: ");
 		this.firstName = in.nextLine();
-		
+
 		System.out.print("Enter student last name:  ");
 		this.lastName = in.nextLine();
-		
+
 		System.out.print("1- First Year \n2- Second Year\n3- Placement\n4- Final Year\nEnter student year: ");
 		this.uniYear = in.nextInt();
-		
+
 		setStudentID();
-		
-		System.out.println(firstName + " " + lastName + " " + uniYear + " " + studentID);
+
 	}
-	//Generate an ID
+
+	// Generate an ID
 	private void setStudentID() {
-		//uni year + id
+		// uni year + id
 		id++;
 		this.studentID = uniYear + "" + id;
-		
-		
+
 	}
-	
-	//enrol in courses
-	
-	//view balance
-	
-	//pay tuition
-	
-	//show status
-	
+
+	// enrol in courses
+	public void enroll() {
+		// Get inside a loop, user hits 0 when done enrolling
+		do {
+			System.out.print("Enter a course to enroll (Q to quit):");
+			Scanner in = new Scanner(System.in);
+			String course = in.nextLine();
+			if (!course.equals("Q")) {
+				courses = courses + "\n" + course;
+				tuitionBalance = tuitionBalance + costOfCourse;
+			} else {
+				break;
+			}
+		} while (1 != 0);
+
+	}
+
+	// view balance
+	public void viewBalance() {
+		System.out.println("Your balance is £" + tuitionBalance);
+	}
+
+	// pay tuition
+	public void payTuition() {
+		viewBalance();
+		System.out.print("Enter your payment: £");
+		Scanner in = new Scanner(System.in);
+		int payment = in.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for your payment of £" + payment);
+		viewBalance();
+	}
+
+	// show status
+	public String toString() {
+		return "Name: " + firstName + lastName + "\nStudent ID: " + studentID + "\nYear: " + uniYear
+				+ "\n  Courses Enrolled: " + courses + "\nBalance: £" + tuitionBalance;
+	}
+
 }
--- JavaProjects/src/studentdatabaseapp/StudentDatabaseApp.java
+++ JavaProjects/src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,12 +1,26 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
-		Student stu1 = new Student();
-		//ask how many users we want to ass
-		
-		//Create a number of new students
+
+		// ask how many students we want to ass0
+		System.out.println("Enter number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
+
+		// Create a number of new students
+		for (int n = 0; n < numOfStudents; n++) {
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
+		for (int n = 0; n < numOfStudents; n++) {
+			System.out.println(students[n].toString());
+		}
 	}
 
 }
